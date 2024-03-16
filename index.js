00a196209c42ae705d074fa16ed311f6046e8b89
Rejoy01 <rejoywilson65@gmail.com>
2024-03-16 11:45:43 +0530

    server connection

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 const app = express()
+
+//midlleware
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+dotenv.config()
+
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+.then(()=>app.listen(process.env.PORT,()=>{console.log(`listening at ${process.env.PORT}`)}))
+.catch((error)=>console.log(error))
+
